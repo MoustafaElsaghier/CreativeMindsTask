7e33f9c38b868333ae1b76caefa4a26d696aab54
mes <mes@dineoutbahrain.com>
2020-09-21 20:36:38 +0200

    finalize task

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -1,8 +1,12 @@
 package com.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.test.Activities.ReposListActivity;
+import com.test.Activities.UiTaskActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.apiTask).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ReposListActivity.class)));
+        findViewById(R.id.uiTask).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, UiTaskActivity.class)));
     }
 }
